1be8887842f3d05c5d84a223a0a00bc3175e3723
morroisback <lvov.nikita@icloud.com>
2025-10-30 21:37:37 +0300

    Уточнены аннотации типов в классе AsyncWorker для методов инициализации, запуска и остановки задач. Улучшена читаемость кода.

--- exchange_observer/utils/async_worker.py
+++ exchange_observer/utils/async_worker.py
@@ -1,29 +1,31 @@
 import asyncio
 import threading
 
+from concurrent.futures import Future
+
 from exchange_observer.core.interfaces import IAsyncTask
 
 
 class AsyncWorker(threading.Thread):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(daemon=True)
         self.loop = None
 
-    def run(self):
+    def run(self) -> None:
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
         self.loop.run_forever()
 
-    def start_task(self, task: IAsyncTask):
+    def start_task(self, task: IAsyncTask) -> Future | None:
         if self.loop:
             return asyncio.run_coroutine_threadsafe(task.start(), self.loop)
         return None
 
-    def stop_task(self, task: IAsyncTask):
+    def stop_task(self, task: IAsyncTask) -> Future | None:
         if self.loop:
             return asyncio.run_coroutine_threadsafe(task.stop(), self.loop)
         return None
 
-    def stop_loop(self):
+    def stop_loop(self) -> None:
         if self.loop:
             self.loop.call_soon_threadsafe(self.loop.stop)
